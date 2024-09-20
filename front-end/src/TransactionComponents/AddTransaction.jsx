0b70d4b17f4effd4fe0180af5c0e499d214ce06e
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:48:43 +0530

    Add Transaction Component

--- front-end/src/TransactionComponents/AddTransaction.jsx
+++ front-end/src/TransactionComponents/AddTransaction.jsx
@@ -16,59 +16,64 @@ function AddTransaction({ fetchUserData }) {
     newData[name] = value;
     setTransaction(newData);
   };
-  const addExpense = async () => {
+  const addExpense = async (e) => {
+    e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:8000/expense",
+        `${process.env.REACT_APP_BACKEND_URL}/expense`,
         transaction,
         { withCredentials: true }
       );
-      console.log("from addExpense ", response.data);
+      setTransaction({ text: "", amount: "" });
       toast.success(response.data.message);
       fetchUserData();
     } catch (error) {
       if (error.status === 400) {
         toast.error(error.response.data.error);
-        console.log("Error from addExpense 400", error.response.data.error);
-      }
-      else if (error.status === 404) {
+      } else if (error.status === 404) {
         toast.error(error.response.data.message);
-      }
-      else if (error.status === 500) {
+      } else if (error.status === 500) {
         toast.error(error.response.data.message);
-      }
-      else {
+      } else {
         toast.error("Some Error Occured");
       }
     }
   };
 
   return (
-    <div className="w-full">
-      <label htmlFor="text">Expense Description</label>
-      <Input className="w-full"
-        type="text"
-        name="text"
-        placeholder="Enter Description"
-        value={transaction.expense}
-        onChange={handleChange}
-      ></Input>
-      <label htmlFor="amount">Enter Amount</label>
-      <p>(negative - expense, positive + income)</p>
-      <Input
-        type="number"
-        name="amount"
-        placeholder="Enter Amount"
-        value={transaction.amount}
-        onChange={handleChange}
-      ></Input>
-      <button
-        className="w-full bg-sky-500 text-white py-2 rounded-md hover:bg-cyan-600"
-        onClick={addExpense}
-      >
-        Add Expense
-      </button>
-      <ToastContainer />
+    <div className="w-full mt-5">
+      <form onSubmit={addExpense} noValidate>
+        <label htmlFor="text" className="font-semibold text-sm text-white">
+          Expense Description
+        </label>
+
+        <Input
+          className="w-full bg-gray-700 font-semibold  placeholder:text-gray-400 placeholder:text-sm text-white  border border-zinc-800 rounded p-1 mb-2 focus:outline-none focus:ring-2 focus:ring-slate-800"
+          type="text"
+          name="text"
+          placeholder="Enter Description"
+          value={transaction.text}
+          onChange={handleChange}
+        ></Input>
+
+        <label htmlFor="amount" className="font-semibold text-sm text-white">
+          Enter Amount
+        </label>
+        <p className="font-semibold text-xs mb-2 text-white">
+          (negative - expense, positive + income)
+        </p>
+        <Input
+          className="w-full bg-gray-700 font-semibold  placeholder:text-gray-400 placeholder:text-sm text-white  border border-zinc-800 rounded p-1 mb-2 focus:outline-none focus:ring-2 focus:ring-slate-800"
+          type="number"
+          name="amount"
+          placeholder="Enter Amount"
+          value={transaction.amount}
+          onChange={handleChange}
+        ></Input>
+        <button className="w-full bg-blue-500 text-white font-bold py-2 rounded-md hover:bg-blue-600">
+          Add Expense
+        </button>
+      </form>
     </div>
   );
 }
