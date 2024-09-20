6af6354ce1314421ee85f988a5d9d162637f0bc2
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:46:09 +0530

    Input field component

--- front-end/src/Components/Input.jsx
+++ front-end/src/Components/Input.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-function Input({ type, placeholder, value, onChange, label,name }) {
+function Input({ type, placeholder, value, onChange, label,name,className }) {
   return (
     <div>
      
       <input
+        className={className}
         name={name}
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         required
+        autoComplete='off'
       />
     </div>
   )
