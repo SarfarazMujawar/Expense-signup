e66df262a1109900828dbec1a1eaac590e5c2dbe
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:49:37 +0530

    Header User Component

--- front-end/src/TransactionComponents/Header.jsx
+++ front-end/src/TransactionComponents/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header({user}) {
   return (
-    <div className="flex justify-start items-start w-full px-4 "> {/* Ensure this takes full width */}
-    <h2 className="text-xl font-bold w-full">Welcome, {user.name}</h2>
+    <div className="flex justify-start items-start w-full  "> {/* Ensure this takes full width */}
+    <h2 className="text-xl text-white font-bold w-full">Welcome, {user.name}</h2>
   </div>
   )
 }
