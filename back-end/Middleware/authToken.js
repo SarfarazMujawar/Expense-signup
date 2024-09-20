29484a763665c34e6b31b0ca846afe3354681dea
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:35:58 +0530

    JWT Token Validadtion

--- back-end/Middleware/authToken.js
+++ back-end/Middleware/authToken.js
@@ -5,7 +5,7 @@ const authToken = (req,res,next)=>{
     if (!token) {
         return res.status(401).json({ message: 'No token provided, authorization denied.' });
       }
-
+      
       try{
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.user=decoded;
