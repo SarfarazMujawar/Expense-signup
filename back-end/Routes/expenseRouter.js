0a0adeea23c0d7fb212629a6162a07ee5cabc0dd
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:43:56 +0530

    Expense Router

--- back-end/Routes/expenseRouter.js
+++ back-end/Routes/expenseRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {fetchExpense, addExpense,deleteExpense} = require('../Controller/ExpenseController');
 const {expenseValidation} = require('../Middleware/expenseValidation');
 
-//to fetch all transactions of user
+
 router.get('/',authToken,fetchExpense);
 router.post('/',authToken,expenseValidation,addExpense);
 router.delete('/:expenseID',authToken,deleteExpense);
