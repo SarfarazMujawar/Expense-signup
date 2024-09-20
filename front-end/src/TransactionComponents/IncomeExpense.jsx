a90ee4081ab4c1b036bf50182ea2cedf0b5e4713
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:50:10 +0530

    User income expense component

--- front-end/src/TransactionComponents/IncomeExpense.jsx
+++ front-end/src/TransactionComponents/IncomeExpense.jsx
@@ -17,17 +17,16 @@ function IncomeExpense({transactions}) {
         return acc+ele
 
     },0)*-1;
-    console.log('icnome',income);
-    console.log('expense',expense);
+   
     
   return (
-    <div className='w-full grid grid-cols-2 bg-zinc-500 gap-6  mt-5 px-4'>
-        <div className='max-w-xs bg-green-300 border-l-8 border-green-500 rounded-md p-2'>
-            <h1 className='font-semibold text-sm'>Income</h1>
+    <div className='w-full grid grid-cols-2  bg-none rounded-lg gap-6  mt-5 '>
+        <div className='max-w-xs bg-green-300 border-l-8 border-green-500 rounded-md p-2 shadow-lg shadow-gray-800'>
+            <h1 className='font-semibold text-sm text-green-800'>Income</h1>
             <p className='font-bold text-xl text-green-800'>{formatAmount(income)}</p>
         </div>
-        <div className='bg-red-300 max-w-xs  border-l-8 border-red-500 rounded-md p-2'>
-            <h1 className='font-semibold text-sm'>Expense</h1>
+        <div className='bg-red-300 max-w-xs  border-l-8 border-red-500 rounded-md p-2 shadow-lg shadow-gray-800'>
+            <h1 className='font-semibold text-sm text-red-800'>Expense</h1>
             <p  className='font-bold text-xl text-red-800'> {formatAmount(expense)}</p>
         </div>
       
