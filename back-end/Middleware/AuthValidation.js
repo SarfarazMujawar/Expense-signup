18ff61110104e7fd777ded97fd199cc287d2247a
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:28:06 +0530

    Auth Validadtion Formatting

--- back-end/Middleware/AuthValidation.js
+++ back-end/Middleware/AuthValidation.js
@@ -1,30 +1,28 @@
 const Joi = require('joi');
 
-const loginValidation =(req,res,next)=>{
+const loginValidation = (req, res, next) => {
 
     const schema = Joi.object({
-        email:Joi.string().email().required(),
-        password:Joi.string().min(4).max(100).required()
+        email: Joi.string().email().required(),
+        password: Joi.string().min(4).max(100).required()
     });
-    const {error} = schema.validate(req.body);
-    if(error)
-    {
-        return res.status(400).json({message:"Bad Request",error: error.details[0].message});
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ message: "Bad Request", error: error.details[0].message });
     }
     next();
 }
-const signupValidation = (req,res,next)=>{
+const signupValidation = (req, res, next) => {
     const schema = Joi.object({
-        name:Joi.string().required().min(2).max(100).required(),
-        email:Joi.string().email().required(),
-        password:Joi.string().min(4).max(100).required()
+        name: Joi.string().required().min(2).max(100).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(4).max(100).required()
     })
-    const {error} = schema.validate(req.body);
-    if(error)
-    {
-        return res.status(400).json({message:"Bad Request",error: error.details[0].message});
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ message: "Bad Request", error: error.details[0].message });
     }
     next();
 }
 
-module.exports ={loginValidation,signupValidation};
+module.exports = { loginValidation, signupValidation };
