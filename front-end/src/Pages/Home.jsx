a9ea3e359139be664a10cc2ff813c0846595c88a
Sarfaraz Mujawar <sarfarazmujawar33@gmail.com>
2024-09-20 22:47:11 +0530

    Home Page

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useEffect } from "react";
 
+
 import { UserContext } from "../Context/UserContext";
 import Balance from "../TransactionComponents/Balance";
 import IncomeExpense from "../TransactionComponents/IncomeExpense";
 import AddTransaction from "../TransactionComponents/AddTransaction";
 import Navbar from "../TransactionComponents/Navbar";
 import Header from "../TransactionComponents/Header";
-
+import Passbook from "../TransactionComponents/Passbook";
 function Home({ setIsAuthenticated }) {
   const {
     user,
@@ -14,11 +15,11 @@ function Home({ setIsAuthenticated }) {
     setUser,
     setTransactions,
     fetchUserData,
-    addTransaction,
+  
     deleteTransaction,
   } = useContext(UserContext);
 
-  console.log(user);
+
 
   useEffect(() => {
     fetchUserData();
@@ -26,22 +27,31 @@ function Home({ setIsAuthenticated }) {
   if (!user) {
     return <div>Loading...</div>; // or a suitable loading indicator
   }
-  console.log('transations',transactions);
+ 
 
   return (
-    <div className="w-full bg-zinc-500">
+    <div className="w-full min-h-screen  bg-gradient-to-br from-gray-900 via-gray-700 to-black  ">
   <Navbar
     setUser={setUser}
     setTransactions={setTransactions}
     setIsAuthenticated={setIsAuthenticated}
   />
-  <div className="flex flex-col justify-center items-center max-w-xs mx-auto mt-6  "> {/* Center content with a max width */}
+  <div className="grid grid-cols-1  mt-2 md:grid-cols-2">
+
+  
+  <div className="flex flex-col justify-center items-center min-w-80 max-w-80 mx-auto bg-white bg-opacity-20 backdrop-blur-lg rounded-lg mt-3 px-3 py-2 shadow-xl shadow-black "> {/* Center content with a max width */}
     
     <Header user={user}/>
     <Balance transactions={transactions} />
     <IncomeExpense transactions={transactions} />
     
     <AddTransaction fetchUserData={fetchUserData} />
+  </div>
+
+  <div>
+    <Passbook transactions={transactions} deleteTransaction={deleteTransaction}/>
+            
+  </div>
   </div>
 </div>
   );
